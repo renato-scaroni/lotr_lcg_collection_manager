c618c5c89aa80d9faf49a4ff1ad1e40d0d23a3f0
scaroni <renato.scaroni@gmail.com>
2023-08-14 15:27:10 -0300

    including file upload

--- deck_analysis/app.py
+++ deck_analysis/app.py
@@ -9,76 +9,46 @@ from cards_map_page import CardsMapPage
 
 pn.extension('tabulator', template='fast')
 
-file_input = pn.widgets.FileInput(
-    accept='.zip'
-)
-
-decks = pn.Tabs()
-
-def extract_file(file_name):
-    with ZipFile(f'tmp/{file_name}.zip', 'r') as zipObj:
-        zipObj.extractall(f'tmp/{file_name}')
-
-class ProxiesPage(pn.Column):
-    def __init__(self, df):
-        self.df = df
-        self.df_obj = pn.pane.DataFrame(df, width=400)
+class MainApp(pn.Column):
+    def __init__(self):
+        self.main_area = pn.Tabs()
+        self.file_input = pn.widgets.FileInput(
+            accept='.zip'
+        )
+        self.file_input.param.watch(self.save_file, 'value')
+        
+        
         super().__init__(
-            '# Proxies', 
-            self.df_obj, 
-            name='proxies')
+            '# Deck Analysis',
+            self.file_input,
+            '# Escolha um arquivo com decks para analisar',
+            '(arquivo .zip com decks em formato .txt como exportado pelo RingsDB))',
+            self.main_area
+       )
+        
+    def save_file(self, event):
+        self.file_input.loading = True
+        os.makedirs('tmp', exist_ok=True)
+        self.file_input.loading = True
+        ts = datetime.now().strftime("%Y%m%d%H%M%S")
+        file_name = f"decks_{ts}"
+        self.file_input.save(f'tmp/{file_name}.zip')
+        with ZipFile(f'tmp/{file_name}.zip', 'r') as zipObj:
+            zipObj.extractall(f'tmp/{file_name}')
 
-# file_input.loading = True
-def save_file(event):
-    os.makedirs('tmp', exist_ok=True)
-    file_input.loading = True
-    ts = datetime.now().strftime("%Y%m%d%H%M%S")
-    file_name = f"decks_{ts}"
-    file_input.save(f'tmp/{file_name}.zip')
-    extract_file(file_name)
+        df = self.load_decklists(f'tmp/{file_name}')
+        cards_map = CardsMapPage(df)
+        self.main_area.append(cards_map)
 
-    df = load_decklists(f'tmp/{file_name}')
-    loaded = DeckListPage('Decks carregados', df)
-    decks.append(loaded)
+        loaded = DeckListPage('Decks carregados', df)
+        self.main_area.append(loaded)
 
-    cards_map = CardsMapPage(df)
-    decks.append(cards_map)
+        self.file_input.loading = False
 
-    file_input.loading = False
-
-def load_decklists(path):
-    decks = du.load_decks(path)
-    decklist_df = du.cards_in_decks(du.count_cards_in_deck, decks)
-    return decklist_df
-
-file_input.param.watch(save_file, 'value')
-
-
-
-# get_most_simultanious_deck(cards_map, 5) + get_most_simultanious_deck(cards_map, 6)
+    def load_decklists(self, path):
+        decks = du.load_decks(path)
+        decklist_df = du.cards_in_decks(du.count_cards_in_deck, decks)
+        return decklist_df
 
 
-class ProxiesPage(pn.Column):
-    def __init__(self, df):
-        self.df = df
-        self.df_obj = pn.pane.DataFrame(df, width=400)
-        super().__init__(
-            '# Proxies', 
-            self.df_obj, 
-            name='proxies')
-
-df = load_decklists(f'tmp/ringsdb')
-# loaded = DeckListPage('Decks carregados', df)
-# decks.append(loaded)
-
-cards_map = CardsMapPage(df)
-decks.append(cards_map)
-
-pn.Column(
-    # '# Escolha um arquivo com decks para analisar',
-    # file_input,
-    # '(arquivo .zip com decks em formato .txt como exportado pelo RingsDB))',
-    decks
-).servable(target='main')
-
-
+MainApp().servable(target='main')
